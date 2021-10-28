52e090a5b2fd33fdc279d73601c4b338f6c00496
心定 <zonglin.nzl@alibaba-inc.com>
2021-10-28 12:48:47 +0800

    feat:增加一段okhttp dns的同步接口实现

--- app/src/main/java/com/aliyun/ams/httpdns/demo/MainActivity.java
+++ app/src/main/java/com/aliyun/ams/httpdns/demo/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private TextView tvConsole;
 
 
-    private static HttpDnsService httpdns;
+    public static HttpDnsService httpdns;
     private static ExecutorService pool = Executors.newSingleThreadExecutor();
 
     private Handler mHandler = null;
---
+++ app/src/main/java/com/aliyun/ams/httpdns/demo/OkHttpDns.java
@@ -0,0 +1,44 @@
+package com.aliyun.ams.httpdns.demo;
+
+import com.alibaba.sdk.android.httpdns.HTTPDNSResult;
+import com.alibaba.sdk.android.httpdns.HttpDnsService;
+import com.alibaba.sdk.android.httpdns.RequestIpType;
+import com.alibaba.sdk.android.httpdns.SyncService;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.Dns;
+
+/**
+ * 示意代码
+ *
+ * @author zonglin.nzl
+ * @date 10/28/21
+ */
+public class OkHttpDns implements Dns {
+    private HttpDnsService service = MainActivity.httpdns;
+
+    @NotNull
+    @Override
+    public List<InetAddress> lookup(@NotNull String s) throws UnknownHostException {
+        SyncService syncService = (SyncService) service;
+
+        // 请求的ip类型根据 网络环境判断
+        HTTPDNSResult result = syncService.getByHost(s, RequestIpType.v4);
+        // 如果请求的 v4 使用 result.getIps()，如果请求的v6 使用 result.getIpv6s()
+        if (result != null && result.getIps() != null && result.getIps().length > 0) {
+            ArrayList<InetAddress> list = new ArrayList<>();
+            for (int i = 0; i < result.getIps().length; i++) {
+                list.addAll(Arrays.asList(InetAddress.getAllByName(result.getIps()[i])));
+            }
+            return list;
+        }
+        return Dns.SYSTEM.lookup(s);
+    }
+}
