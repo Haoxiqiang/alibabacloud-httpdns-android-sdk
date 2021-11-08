048b2e20a9a4e249659aaa0e3e0d1b06af61f9a0
心定 <zonglin.nzl@alibaba-inc.com>
2021-11-08 19:37:02 +0800

    test:调整测试case

--- app/src/test/java/com/aliyun/ams/httpdns/demo/HttpDnsE2E.java
+++ app/src/test/java/com/aliyun/ams/httpdns/demo/HttpDnsE2E.java
@@ -1,7 +1,5 @@
 package com.aliyun.ams.httpdns.demo;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import android.Manifest;
 import android.net.ConnectivityManager;
 
@@ -37,6 +35,8 @@ import org.robolectric.shadows.ShadowLog;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.CountDownLatch;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * @author zonglin.nzl
@@ -854,6 +854,7 @@ public class HttpDnsE2E {
 
     @Test
     public void testIpTtl() throws InterruptedException {
+        app.enableExpiredIp(false);
         InterpretHostResponse response = ServerHelper.randomInterpretHostResponse(app.getRequestHost(), 1);
         server.getInterpretHostServer().preSetRequestResponse(app.getRequestHost(), response, -1);
         // 请求域名解析，并返回空结果，因为是接口是异步的，所以第一次请求一个域名返回是空
@@ -880,7 +881,7 @@ public class HttpDnsE2E {
 
     @Test
     public void testEnableExpiredIp() throws InterruptedException {
-        app.enableExpiredIp();
+        app.enableExpiredIp(true);
         InterpretHostResponse response = ServerHelper.randomInterpretHostResponse(app.getRequestHost(), 1);
         InterpretHostResponse response1 = ServerHelper.randomInterpretHostResponse(app.getRequestHost());
         server.getInterpretHostServer().preSetRequestResponse(app.getRequestHost(), response, 1);
@@ -1038,6 +1039,7 @@ public class HttpDnsE2E {
     @Test
     public void testIpCacheWhenExpired() throws InterruptedException {
         app.enableCache(false);
+        app.enableExpiredIp(false);
         // 先发起一些请求，缓存一些Ip结果
         InterpretHostResponse response = ServerHelper.randomInterpretHostResponse(app.getRequestHost(), 1);
         InterpretHostResponse response1 = ServerHelper.randomInterpretHostResponse(app.getRequestHost(), 1);
@@ -1058,6 +1060,7 @@ public class HttpDnsE2E {
         // 重启应用，获取新的实例
         app.start(new HttpDnsServer[]{server, server1, server2}, speedTestServer);
         app.enableCache(false);
+        app.enableExpiredIp(false);
 
         ips = app.requestInterpretHost();
         ServerStatusHelper.hasReceiveAppInterpretHostRequestWithResult("本地缓存过期时，会触发网络请求", app, server, response1);
