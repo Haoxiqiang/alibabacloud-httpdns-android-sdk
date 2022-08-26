bcfff44c029393aee39b16f7df37d3957e80c63e
心定 <zonglin.nzl@alibaba-inc.com>
2022-08-26 15:52:53 +0800

    test:增加模拟ipv6服务的能力

--- httpdns-sdk/src/end2end/java/com/alibaba/sdk/android/httpdns/test/server/HttpDnsServer.java
+++ httpdns-sdk/src/end2end/java/com/alibaba/sdk/android/httpdns/test/server/HttpDnsServer.java
@@ -6,6 +6,9 @@ import com.alibaba.sdk.android.httpdns.test.server.base.RequestListener;
 import com.alibaba.sdk.android.httpdns.test.utils.TestLogger;
 
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
 import java.util.HashMap;
 
 import okhttp3.mockwebserver.MockWebServer;
@@ -31,10 +34,17 @@ public class HttpDnsServer {
     private ServerIpsServer serverIpsServer;
     private DebugApiServer debugApiServer;
 
+    private boolean v6 = false;
+    private String ipv6;
+
     /**
      * 服务启动
      */
     public void start() {
+        start(false);
+    }
+
+    public void start(boolean v6) {
         interpretHostServer = new InterpretHostServer(secretServce);
         resolveHostServer = new ResolveHostServer(secretServce);
         serverIpsServer = new ServerIpsServer();
@@ -44,13 +54,31 @@ public class HttpDnsServer {
         servers.put(REQUEST_TYPE_DEBUG_REQUEST, debugApiServer);
         servers.put(REQUEST_TYPE_RESOLVE_REQUEST, resolveHostServer);
         try {
-            mockWebServer.start();
+            mockWebServer.start(getAddress(v6), 0);
             mockWebServer.setDispatcher(requestHandler);
             requestHandler.setServers(servers);
         } catch (IOException e) {
             e.printStackTrace();
         }
         TestLogger.log(this.toString() + " start " + mockWebServer.toString());
+    }
+
+    private InetAddress getAddress(boolean v6) throws IOException {
+        InetAddress[] addresses = InetAddress.getAllByName("localhost");
+        for (int i = 0; i < addresses.length; i++) {
+            if (v6) {
+                if (addresses[i] instanceof Inet6Address) {
+                    this.v6 = true;
+                    this.ipv6 = addresses[i].getHostAddress();
+                    return addresses[i];
+                }
+            } else {
+                if (addresses[i] instanceof Inet4Address) {
+                    return addresses[i];
+                }
+            }
+        }
+        throw new IOException("no request address!");
     }
 
     /**
@@ -70,6 +98,9 @@ public class HttpDnsServer {
      * @return
      */
     public String getServerIp() {
+        if (v6) {
+            return ipv6;
+        }
         return mockWebServer.getHostName();
     }
 
